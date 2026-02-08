cee7eff19c957a86a0d49165063f61621f9238d0
HiteshLohar <loharhitesh2002.com>
2026-02-08 14:54:17 +0530

    changes to postman

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,13 +1,13 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { ApiError } from "../utils/ApiError.js"
-import {User} from "../models/user.models.js"
+import { User } from "../models/user.models.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 const registerUser = asyncHandler(async (req, res) => {
 
     const { username, fullName, email, password } = req.body;
-    console.log("User data : ", req.body);
+    // console.log("User data : ", req.body);
 
 
     if ([fullName, email, password, username].some((field) => field?.trim() === "")) {
@@ -16,46 +16,50 @@ const registerUser = asyncHandler(async (req, res) => {
 
     const existedUser = await User.findOne({ $or: [{ email }, { username }] });
 
-    if(existedUser){
-        throw new ApiError(409,"User With email or username already exists")
+    if (existedUser) {
+        throw new ApiError(409, "User With email or username already exists")
     }
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files?.coverImage[0]?.path;
+    }
     // console.log("Avatar Local Path : ", avatarLocalPath);
     // console.log("Cover Image Local Path : ", coverImageLocalPath);
 
 
-    if(!avatarLocalPath){
-        throw new ApiError(400,"Avatar is required");
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "Avatar is required");
     }
 
     const avatar = await uploadOnCloudinary(avatarLocalPath);
     const coverImage = await uploadOnCloudinary(coverImageLocalPath);
 
-    if(!avatar){
-        console.log("Error while uploading avatar on cloudinary",avatar);
-        throw new ApiError(400,"Avatar file is required");
+    if (!avatar) {
+        console.log("Error while uploading avatar on cloudinary", avatar);
+        throw new ApiError(400, "Avatar file is required");
     }
 
 
     const user = await User.create({
         fullName,
-        avatar:avatar?.url,
-        coverImage:coverImage?.url || "",
+        avatar: avatar?.url,
+        coverImage: coverImage?.url || "",
         email,
         password,
-        username:username.toLowerCase()
+        username: username.toLowerCase()
     })
 
     const createdUser = await User.findById(user._id).select("-password -refreshToken")
 
-    if(!createdUser){
-        throw new ApiError(500,"Something went wrong while registering the user")
+    if (!createdUser) {
+        throw new ApiError(500, "Something went wrong while registering the user")
     }
 
     return res.status(201).json(
-        new ApiResponse(200,createdUser,"User registered Successfully")
+        new ApiResponse(200, createdUser, "User registered Successfully")
     )
 
 
